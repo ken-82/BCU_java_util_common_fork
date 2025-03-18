48d369d3e4fa39b9dd5c5417aa92905a6b13b36b
super cat maker <124401935+ramen-cat-coder@users.noreply.github.com>
2025-03-18 02:24:18 -0700

    Revert "Fix surge level bug"

--- battle/attack/AttackVolcano.java
+++ battle/attack/AttackVolcano.java
@@ -2,59 +2,68 @@ package common.battle.attack;
 
 import common.battle.entity.AbEntity;
 import common.battle.entity.Entity;
+import common.util.Data;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.*;
 
 public class AttackVolcano extends AttackAb {
 	public ContVolcano handler;
-
 	protected boolean attacked = false;
-
 	protected final List<Entity> vcapt = new ArrayList<>();
 
-	private byte volcTime = 40;
+	private byte volcTime = VOLC_ITV;
 
 	public AttackVolcano(Entity e, AttackAb a, float sta, float end, int vt) {
 		super(e, a, sta, end, false);
-		this.isCounter = a.isCounter;
+		isCounter = a.isCounter;
 		this.sta = sta;
 		this.end = end;
 		this.waveType = vt;
 	}
 
+	@Override
 	public void capture() {
-		List<AbEntity> le = this.model.b.inRange(this.touch, -this.dire, this.sta, this.end, this.excludeRightEdge);
-		this.capt.clear();
-		for (AbEntity e : le) {
-			if (e instanceof Entity && !this.vcapt.contains(e))
-				this.capt.add(e);
-		}
+		List<AbEntity> le = model.b.inRange(touch, -dire, sta, end, excludeRightEdge);
+
+		capt.clear();
+
+		for (AbEntity e : le)
+			if (e instanceof Entity && !vcapt.contains((Entity) e)) {
+				capt.add(e);
+			}
 	}
 
+	@Override
 	public void excuse() {
 		process();
-		this.volcTime = (byte)(this.volcTime - 1);
-		if (this.volcTime == 0) {
-			this.volcTime = (byte)20;
-			this.vcapt.clear();
+
+		volcTime--;
+		if (volcTime == 0) {
+			volcTime = VOLC_ITV;
+			vcapt.clear();
 		}
-		this.atk = this.rawAtk;
-		if (this.attacker != null) {
-			if (this.attacker.status[24][0] != 0)
-				this.atk += this.atk * this.attacker.status[24][0] / 100;
-			if (this.attacker.status[8][0] != 0)
-				this.atk = this.atk * this.attacker.status[8][1] / 100;
-			if (this.attacker.dire == 1 && this.attacker.basis.canon.deco == 4)
-				this.atk = (int)(this.atk * this.attacker.basis.b.t().getDecorationMagnification(this.attacker.basis.canon.deco, 3));
+
+		atk = rawAtk;
+
+		if (attacker != null) {
+			if (attacker.status[P_STRONG][0] != 0)
+				atk += atk * attacker.status[P_STRONG][0] / 100;
+			if (attacker.status[P_WEAK][0] != 0)
+				atk = atk * attacker.status[P_WEAK][1] / 100;
+			if (attacker.dire == 1 && attacker.basis.canon.deco == DECO_BASE_WATER)
+				atk *= attacker.basis.b.t().getDecorationMagnification(attacker.basis.canon.deco, Data.DECO_SURGE);
 		}
-		for (AbEntity e : this.capt) {
+
+		for (AbEntity e : capt) {
 			if (e.isBase() && !(e instanceof Entity))
 				continue;
+
 			if (e instanceof Entity) {
 				e.damaged(this);
-				this.attacked = true;
-				this.vcapt.add((Entity)e);
+				attacked = true;
+
+				vcapt.add((Entity) e);
 			}
 		}
 	}
